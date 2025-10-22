d7f38e35f08530e589d4535f8b52e06f6c83f8d8
Nam Dau <namdau14@gmail.com>
2025-10-21 21:52:53 -0400

    add initial boruta skeleton code

---
+++ feature_classification/boruta.py
@@ -0,0 +1,27 @@
+from sklearn.datasets import load_diabetes
+from sklearn.ensemble import RandomForestRegressor
+from boruta import BorutaPy
+import pandas as pd
+import numpy as np
+
+# TODO: load dataset and define them in terms of X and y
+
+# TODO: decide random forest parameters after tuning
+model = RandomForestRegressor(n_estimators=100, max_depth=5, random_state=42)
+
+# using python's Boruta's algorithm
+feat_selector = BorutaPy(
+    verbose=2,
+    estimator=model,
+    n_estimators='auto',
+    max_iter=10,
+    random_state=42,
+)
+
+feat_selector.fit(np.array(X), np.array(y))
+
+X_trained = feat_selector.transform(np.array(X))
+
+model.fit(X_filtered, y)
+
+predictions = model.predict(X_filtered)
